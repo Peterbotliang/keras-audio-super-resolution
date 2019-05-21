1bb8d5ab9f138f0309131ecbe533f8440b37c621
PeterbotLiang <peterbotliang@gmail.com>
2019-05-21 22:54:57 +0000

    install list

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+apt-get install python3.6
+pip install jupyter
+pip install librosa
+pip install tqdm
+pip install tensorboard
+pip install jupyter-tensorboard
+pip3 install tensorflow-gpu==1.13.1
+pip install matplotlib
+pip install soundfile
+apt-get install ffmpeg -y
+apt-get install libsndfile1 -y
