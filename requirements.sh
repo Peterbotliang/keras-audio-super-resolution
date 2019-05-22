a3c6e4a7772506c6c3408e529261924bb0a95190
PeterbotLiang <peterbotliang@gmail.com>
2019-05-22 04:21:18 +0000

    new

--- requirements.sh
+++ requirements.sh
@@ -8,7 +8,9 @@ pip install librosa
 pip install tqdm
 pip install tensorboard
 pip install jupyter-tensorboard
-pip3 install tensorflow-gpu==1.13.1
+pip install tensorflow-gpu==1.13.1
+pip install torch
+pip install torch-vision
 pip install matplotlib
 pip install soundfile
 apt-get install ffmpeg -y
