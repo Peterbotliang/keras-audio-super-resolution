949ec572769e7c2651316cfd2618d72db4fa2108
PeterbotLIang <peterbotliang@gmail.com>
2019-05-24 05:38:59 +0000

    update

--- requirements.sh
+++ requirements.sh
@@ -1,17 +1,15 @@
 apt-get install git
 apt-get install --no-install-recommends libcudnn7=7.5.1.10-1+cuda10.0 libcudnn7-dev=7.5.1.10-1+cuda10.0
-apt-get install python3.6
-wget https://bootstrap.pypa.io/get-pip.py
-python3.6 get-pip.py
-pip install jupyter
-pip install librosa
-pip install tqdm
-pip install tensorboard
-pip install jupyter-tensorboard
-pip install tensorflow-gpu==1.13.1
-pip install torch
-pip install torch-vision
-pip install matplotlib
-pip install soundfile
+apt-get install python3-pip
+pip3 install jupyter
+pip3 install librosa
+pip3 install tqdm
+pip3 install tensorboard
+pip3 install jupyter-tensorboard
+pip3 install tensorflow-gpu==1.13.1
+pip3 install torch
+pip3 install torch-vision
+pip3 install matplotlib
+pip3 install soundfile
 apt-get install ffmpeg -y
 apt-get install libsndfile1 -y
